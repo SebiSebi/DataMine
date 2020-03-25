ae0f250ee4b5fa51a82a1d452d58d78bedadfd73
sebisebi <gpirtoaca@gmail.com>
2020-03-25 18:30:55 +0200

    Added definition of Collection enum with RACE inside it.

--- data_mine/__init__.py
+++ data_mine/__init__.py
@@ -3,3 +3,6 @@ from __future__ import absolute_import
 __author__ = """Pirtoaca George-Sebastian (sebisebi)"""
 __email__ = 'gpirtoaca@gmail.com'
 __version__ = '0.0.1'
+
+
+from .collection import Collection
---
+++ data_mine/collection.py
@@ -0,0 +1,6 @@
+from enum import Enum, unique
+
+
+@unique
+class Collection(Enum):
+    RACE = 1
