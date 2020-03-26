db5ab03fa2b5cd7032e01a0ca7cab872fc04f147
sebisebi <gpirtoaca@gmail.com>
2020-03-26 20:16:45 +0200

    Small docstring bug fix in constants.py

--- data_mine/constants.py
+++ data_mine/constants.py
@@ -1,11 +1,15 @@
-"""
-Various constants used in the project.
+"""Various constants used in the project.
 
 Description:
 
     * DATAMINE_CACHE_DIR_ENV_VAR (str): the name of the environment variable
       whose value dictates where the datasets are downloaded.
 
+    * PROJECT_ROOT (str): the path to the directory containing the main init
+      file (the root of the project).
 """
+import os
+
 
 DATAMINE_CACHE_DIR_ENV_VAR = "DATAMINE_CACHE_DIR"
+PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
