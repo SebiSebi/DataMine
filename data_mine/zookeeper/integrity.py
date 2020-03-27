d8067162f0e045179facb800e5177e923e18a5b4
sebisebi <gpirtoaca@gmail.com>
2020-03-27 15:51:13 +0200

    Added implementation of deep integrity check for dataset local copy.

--- data_mine/zookeeper/integrity.py
+++ data_mine/zookeeper/integrity.py
@@ -3,6 +3,7 @@ import os
 from data_mine import Collection
 from data_mine.constants import PROJECT_ROOT
 from data_mine.utils import datamine_cache_dir
+from data_mine.utils import file_sha256
 from data_mine.zookeeper import load_datasets_config
 from data_mine.zookeeper.download_center import load_config_file
 
@@ -28,7 +29,7 @@ def check_shallow_integrity(dataset_id):
     return True
 
 
-def check_deep_integrity():
+def check_deep_integrity(dataset_id):
     """
     Verifies if the dataset's local copy is valid in a deep way.
 
@@ -41,8 +42,13 @@ def check_deep_integrity():
     Returns:
         result (bool): True if the dataset is valid, False otherwise.
     """
-    pass
-
-
-if __name__ == "__main__":
-    print(check_shallow_integrity(Collection.RACE))
+    assert(isinstance(dataset_id, Collection))
+    config = load_datasets_config()[dataset_id.name]
+    cache_dir = os.path.join(datamine_cache_dir(), dataset_id.name)
+    for sha256, expected_file in load_config_file(os.path.join(PROJECT_ROOT, config["expectedFiles"])):  # noqa: E501
+        expected_file = os.path.join(cache_dir, expected_file)
+        if not os.path.isfile(expected_file):
+            return False
+        if file_sha256(expected_file) != sha256:
+            return False
+    return True
