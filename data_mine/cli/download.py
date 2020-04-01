380ba71bc70618e5edad44251fdc20634c8cbd1d
sebisebi <gpirtoaca@gmail.com>
2020-04-01 12:10:05 +0300

    Added CLI command implementation to download dataset.

--- data_mine/cli/download.py
+++ data_mine/cli/download.py
@@ -1,6 +1,10 @@
+import os
 import sys
 
-from data_mine.utils import msg
+from data_mine import Collection
+from data_mine.utils import datamine_cache_dir, msg
+from data_mine.zookeeper import check_deep_integrity
+from data_mine.zookeeper import download_dataset
 
 
 def download():
@@ -10,6 +14,16 @@ def download():
     if len(sys.argv) != 2:
         msg.error("Usage: python -m data_mine download <dataset_name>", exits=1)  # noqa: E501
     dataset_name = sys.argv[1]
+    if dataset_name not in set([x.name for x in Collection]):
+        msg.error("Invalid dataset: {}".format(dataset_name))
+        msg.info("Available datasets:")
+        msg.info("\n".join([x.name for x in Collection]), exits=1)
 
-    # We perform a deep check of the local copy before downloading.
-    msg.info("Downloading {} ...".format(dataset_name))
+    dataset_id = Collection.from_str(dataset_name)
+    msg.info("Checking if {} is already downloaded ...".format(dataset_name))
+    return_code = download_dataset(dataset_id, check_deep_integrity)
+    if return_code == 1:
+        msg.info("{} already available at: {}".format(
+            dataset_name,
+            os.path.join(datamine_cache_dir(), dataset_name))
+        )
