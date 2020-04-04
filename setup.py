da3b2876ae0971d65c0632b8fb8a387fdbdbf8c2
sebisebi <gpirtoaca@gmail.com>
2020-04-04 21:48:32 +0300

    Added include_package_data=True flag.

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'data_mine',
     ],
     package_dir={'data_mine': 'data_mine'},
+    include_package_data=True,
     install_requires=REQUIREMENTS,
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     keywords='machine learning datasets data collection',
