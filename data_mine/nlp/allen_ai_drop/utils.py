7359e9b5003d817e7da5b867a1fc924774927b5b
sebisebi <gpirtoaca@gmail.com>
2020-04-27 10:55:36 +0300

    Added serialize_date method for DROP dataset.

--- data_mine/nlp/allen_ai_drop/utils.py
+++ data_mine/nlp/allen_ai_drop/utils.py
@@ -16,3 +16,26 @@ def type_to_data_file(drop_type):
             DROPType.TRAIN: os.path.join(data_dir, "drop_dataset_train.json"),
             DROPType.DEV: os.path.join(data_dir, "drop_dataset_dev.json"),
     }[drop_type]
+
+
+def serialize_date(date):
+    """
+    Computes the string representation of a date object composed of "day",
+    "month", and "year". The argument is a Python dictionary. Example:
+    {
+        "day": "24",
+        "month": "September",
+        "year" "1985"
+    }
+    This would lead to the following output: "24 September 1985".
+    Empty fields (day, month or year) are ignored. The only allowed date
+    fields are "day", "month" and "year".
+
+    Returns the string representation of the date object.
+    """
+    assert(set(date.keys()) <= set(["day", "month", "year"]))
+    date = date.items()
+    date = filter(lambda item: item[1] is not None, date)
+    date = filter(lambda item: len(str(item[1])) > 0, date)
+    date = [str(item[1]) for item in sorted(date)]
+    return " ".join(date)
--- tests/nlp/allen_ai_drop/test_utils.py
+++ tests/nlp/allen_ai_drop/test_utils.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 from data_mine.nlp.allen_ai_drop import DROPType
+from data_mine.nlp.allen_ai_drop.utils import serialize_date
 from data_mine.nlp.allen_ai_drop.utils import type_to_data_file
 
 
@@ -15,6 +16,58 @@ class TestUtilFunctions(unittest.TestCase):
 
         self.assertTrue(type_to_data_file(DROPType.TRAIN).endswith("train.json"))  # noqa: E501
         self.assertTrue(type_to_data_file(DROPType.DEV).endswith("dev.json"))  # noqa: E501
+
+    def test_serialize_date(self):
+        tests = [
+                # ((day, month, year), expected_result)
+                (("12", "January", "1995"), "12 January 1995"),
+                (("", "January", "1995"), "January 1995"),
+                (("12", "", "1996"), "12 1996"),
+                (("12", "January", ""), "12 January"),
+                (("", "March", ""), "March"),
+                (("", "", "2020"), "2020"),
+                (("", "", ""), ""),
+                ((0, "April", "2002"), "0 April 2002"),
+                (("0", "April", 0), "0 April 0"),
+                ((0, "June", "0"), "0 June 0"),
+                ((25, "February", 2010), "25 February 2010")
+
+        ]
+        for (day, month, year), expected_result in tests:
+            result = serialize_date({
+                "day": day,
+                "month": month,
+                "year": year
+            })
+            self.assertEqual(result, expected_result)
+
+        # Missing or None fields.
+        self.assertEqual(
+                serialize_date({"day": "12", "year": "1850"}),
+                "12 1850"
+        )
+        self.assertEqual(
+                serialize_date({"day": "30", "month": "July"}),
+                "30 July"
+        )
+        self.assertEqual(
+                serialize_date({"day": None, "month": "September"}),
+                "September"
+        )
+
+        # Different "order".
+        self.assertEqual(
+                serialize_date({"year": 1992, "day": 5, "month": "August"}),
+                "5 August 1992"
+        )
+        self.assertEqual(
+                serialize_date({"month": "March", "year": 1645, "day": "23"}),
+                "23 March 1645"
+        )
+
+        # Invalid fields.
+        with self.assertRaises(AssertionError):
+            serialize_date({"Month": "March"})
 
 
 if __name__ == '__main__':
