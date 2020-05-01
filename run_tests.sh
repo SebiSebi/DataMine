e86451856a8cce3a92436cbc8431fb4dc8a7437a
sebisebi <gpirtoaca@gmail.com>
2020-05-01 20:48:31 +0300

    Use nose to run tests for Python 2.

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-green -vvv --run-coverage tests/
+pyv=$(python -c 'import sys; print(sys.version_info.major)' | sed 's/ *$//g')
+if [ "$pyv" = "3" ]; then
+	green -vvv --run-coverage tests/
+else
+	nosetests --verbosity=2
+fi
