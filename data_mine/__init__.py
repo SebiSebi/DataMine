cf9ec81fd4ecdcf6c272b6cf6541cc51d9358c6d
sebisebi <gpirtoaca@gmail.com>
2020-05-18 12:14:22 +0300

    Bump version: 0.0.8 → 0.0.9

--- data_mine/__init__.py
+++ data_mine/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 __author__ = """Pirtoaca George-Sebastian (sebisebi)"""
 __email__ = 'gpirtoaca@gmail.com'
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 
 from .collection import Collection
