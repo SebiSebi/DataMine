d1e5ffcf991d458701d61b81d3918d6abf9b2326
sebisebi <gpirtoaca@gmail.com>
2020-05-27 10:47:53 +0300

    Added valid_labels function for ARC.

--- data_mine/nlp/allen_ai_arc/utils.py
+++ data_mine/nlp/allen_ai_arc/utils.py
@@ -1,5 +1,7 @@
 import os
 
+from copy import deepcopy
+from six import string_types
 from .constants import ARC_CACHE_DIR
 from .types import ARCType
 
@@ -19,3 +21,27 @@ def type_to_data_file(arc_type):
     basename = split + ".jsonl"
 
     return os.path.join(data_dir, category, basename)
+
+
+def valid_choices(labels):
+    """
+    Returns True/False depending on the labels associated
+    with the correct answers. They must be either numerical
+    or letters but not both digits and letters.
+    """
+    for label in labels:
+        if not isinstance(label, string_types):
+            return False
+        if len(label) != 1:
+            return False
+    labels = tuple(sorted(deepcopy(labels)))
+    if not (3 <= len(labels) <= 5):
+        return False
+    correct = set([
+            ("A", "B", "C"),
+            ("A", "B", "C", "D"),
+            ("A", "B", "C", "D", "E"),
+            ("1", "2", "3"),
+            ("1", "2", "3", "4")
+    ])
+    return labels in correct
--- tests/nlp/allen_ai_arc/test_utils.py
+++ tests/nlp/allen_ai_arc/test_utils.py
@@ -3,6 +3,7 @@ import unittest
 
 from data_mine.nlp.allen_ai_arc import ARCType
 from data_mine.nlp.allen_ai_arc.utils import type_to_data_file
+from data_mine.nlp.allen_ai_arc.utils import valid_choices
 
 
 class TestUtilFunctions(unittest.TestCase):
@@ -47,6 +48,36 @@ class TestUtilFunctions(unittest.TestCase):
             inner_path(type_to_data_file(ARCType.TEST_CHALLENGE)),
             "ARC-Challenge/ARC-Challenge-Test.jsonl"
         )
+
+    def test_valid_choices(self):
+        bad = [
+                [1, "1"],
+                ["1", "2"],
+                ["A"],
+                ["1", "B", "3", "D"],
+                ["A", "B", "D", "E"],
+                ["A", "B", "C", "D", "D"],
+                ["A", "B", "C", "D", "F"],
+                ["B", "C", "D", "E"],
+                ["C", "D", "E"],
+                ["2", "3", "4"],
+                ["1", "2", "3", "4", "5"],
+        ]
+        good = [
+                ["1", "2", "3", "4"],
+                ["1", "2", "3"],
+                ["4", "2", "3", "1"],
+                ["3", "2", "1"],
+                ["A", "B", "C", "D"],
+                ["A", "B", "C", "D", "E"],
+                ["A", "B", "C"],
+                ["D", "C", "B", "A"],
+                ["D", "A", "E", "C", "B"]
+        ]
+        for labels in bad:
+            self.assertFalse(valid_choices(labels))
+        for labels in good:
+            self.assertTrue(valid_choices(labels))
 
 
 if __name__ == '__main__':
